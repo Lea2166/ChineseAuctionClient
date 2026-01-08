929578070011a7fd6b27f01764c96dad9044a1e4
naama_code <naanas344@gmail.com>
2026-01-08 13:54:23 +0200

    small changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule,NzIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
