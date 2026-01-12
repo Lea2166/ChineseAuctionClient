ebcfb407a95da3dc76a2455d8ccdf46fbd883eae
Lea2166 <lea202166@gmail.com>
2026-01-12 18:44:16 +0200

    add prize view & add one prize

--- services/prizes.ts
+++ services/prizes.ts
@@ -12,12 +12,16 @@ export class PrizesService {
 
   private _prizes = signal<ReadPrizeDTO[] | []>([]);
   readonly prizes = computed(() => this._prizes());
-
+  private _simplePrize = signal<CreatePrizeDTO|null>(null);
+  readonly simplePrize = computed(() => this._simplePrize());
   getAllPrizes(): Observable<ReadPrizeDTO[]> {
     return this.http.get<ReadPrizeDTO[]>(`${this.apiUrl}`).pipe(
       tap((prizes: ReadPrizeDTO[]) => this._prizes.set(prizes)))
   }
   setAllPrizes(prizes:ReadPrizeDTO[]):void{
     this._prizes.set(prizes)
+  }
+  setSimplePrize(prize: CreatePrizeDTO): Observable<CreatePrizeDTO> {
+    return this.http.post<CreatePrizeDTO>(`${this.apiUrl}`, prize).pipe();
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Prizes } from '../pages/prizes/prizes';
 import { Donors } from '../pages/donors/donors';
 import { OnePrize } from '../PrizeComponents/one-prize/one-prize';
+import { AddPrize } from '../PrizeComponents/add-prize/add-prize';
 
 
 export const routes: Routes = [
@@ -14,5 +15,6 @@ export const routes: Routes = [
     // ]
   },
   { path: 'prizes/:id', pathMatch: 'full', component: OnePrize },
-  { path: 'donors', pathMatch: 'full', component: Donors }
+  { path: 'donors', pathMatch: 'full', component: Donors },
+  {path: 'prizes/add', pathMatch: 'full', component: AddPrize}
 ];
