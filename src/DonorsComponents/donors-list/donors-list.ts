9dbe6567473a445928138706289ec9ecd55af183
naama_code <naanas344@gmail.com>
2026-01-12 17:35:28 +0200

    add donors list component

---
+++ src/DonorsComponents/donors-list/donors-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donors-list',
+  imports: [],
+  templateUrl: './donors-list.html',
+  styleUrl: './donors-list.scss',
+})
+export class DonorsList {
+
+}
