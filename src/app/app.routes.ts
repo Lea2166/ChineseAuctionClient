3707f412b2ae8702b6e72bf5d49f84a33774dfc5
naama_code <naanas344@gmail.com>
2026-01-19 12:54:23 +0200

    some changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,18 @@ import { Prizes } from '../pages/prizes/prizes';
 import { Donors } from '../pages/donors/donors';
 import { OnePrize } from '../PrizeComponents/one-prize/one-prize';
 import { AddPrize } from '../PrizeComponents/add-prize/add-prize';
+import { Packages } from '../pages/packages/packages';
+import { HomePage } from '../pages/home-page/home-page';
 
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'home', pathMatch: 'full', component: HomePage },
   {
     path: 'prizes', pathMatch: 'full', component: Prizes,
   },
   { path: 'prizes/:id', pathMatch: 'full', component: OnePrize },
   { path: 'donors', pathMatch: 'full', component: Donors },
-  {path: 'prizes/add', pathMatch: 'full', component: AddPrize}
+  { path: 'prizes/add', pathMatch: 'full', component: AddPrize },
+  { path: 'packages', pathMatch: 'full', component: Packages }
+
 ];
