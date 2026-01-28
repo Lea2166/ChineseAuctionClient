a6bce45a2752d946e8e146516b1425acf1040fe0
naama_code <naanas344@gmail.com>
2026-01-28 15:53:06 +0200

    donor filters work

--- src/DonorsComponents/donors-filters-view/donors-filters-view.ts
+++ src/DonorsComponents/donors-filters-view/donors-filters-view.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReadPrizeDTO } from '../../../models/Prize';
+import { DonorQParams } from '../../../models/Filters';
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzSliderModule } from "ng-zorro-antd/slider";
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-donors-filters-view',
-  imports: [],
+  imports: [NzFormModule, NzSliderModule,ReactiveFormsModule, NzButtonModule,NzInputModule,NzIconModule,NzSelectModule],
   templateUrl: './donors-filters-view.html',
   styleUrl: './donors-filters-view.scss',
 })
 export class DonorsFiltersView {
+
+  private fb = inject(FormBuilder);
+
+  filters = this.fb.group({
+    email: this.fb.control<string | null>(null),
+    name: this.fb.control<string | null>(null),
+    prizesIds: this.fb.control<number[]>([]),
+
+  });
+
+  @Input() prizes: ReadPrizeDTO[] = []
+
+
+  @Output() filtersRequest = new EventEmitter<DonorQParams>()
+
+  cancelFilters() {
+    this.filters.reset()
+    this.filtersRequest.emit()
+  }
+
+  applyFilters(): void {
+
+    const filtersForSend: DonorQParams = {
+      email: this.filters.get('email')?.value ?? undefined,
+      name: this.filters.get('name')?.value ?? undefined,
+      prizesIds: this.filters.get('prizesIds')?.value ?? undefined,
+
+    }
+
+    this.filtersRequest.emit(filtersForSend)
+
+  }
 
 }
--- src/DonorsComponents/donors-filters/donors-filters.ts
+++ src/DonorsComponents/donors-filters/donors-filters.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PrizesService } from '../../../services/prizes';
+import { DonorsService } from '../../../services/donors';
+import { UserService } from '../../../services/user';
+import { ReadPrizeDTO } from '../../../models/Prize';
+import { DonorQParams } from '../../../models/Filters';
+import { DonorsFiltersView } from '../donors-filters-view/donors-filters-view';
 
 @Component({
   selector: 'app-donors-filters',
-  imports: [],
+  imports: [DonorsFiltersView],
   templateUrl: './donors-filters.html',
   styleUrl: './donors-filters.scss',
 })
 export class DonorsFilters {
 
+  userService: UserService = inject(UserService);
+  prizesService: PrizesService = inject(PrizesService)
+  donorsService: DonorsService = inject(DonorsService);
+
+  prizes: ReadPrizeDTO[] = []
+
+
+  ngOnInit() {
+
+    const cachedPrizes = this.prizesService.prizes();
+
+    if (cachedPrizes && cachedPrizes.length > 0) {
+      this.prizes = cachedPrizes;
+    } else {
+      this.prizesService.getAllPrizes().subscribe(prizes => {
+        this.prizes = prizes;
+        this.prizesService.setAllPrizes(prizes);
+      });
+    }
+
+
+
+  }
+
+  sendFilters(filters: DonorQParams) {
+    this.donorsService.getAlldonors(this.userService.token(), filters).subscribe({
+      next: donors => {
+        this.donorsService.setDonors([...donors])
+
+
+      },
+      error: (err: any) => {
+        console.error('error fetch prizes with filters', err);
+      }
+    })
+  }
 }
