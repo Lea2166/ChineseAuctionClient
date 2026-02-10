0597fdfb55fa3f1ea376f5041b142485ff3b3dac
Lea2166 <lea202166@gmail.com>
2026-02-10 22:13:29 +0200

    addig the service of Cart

--- services/cart-service.ts
+++ services/cart-service.ts
@@ -13,8 +13,30 @@ export class CartService {
 
   private _cart = signal<ReadCartDTO | null>(null);
   readonly cart = computed(() => this._cart());
-
-
+  AddPrizeToCart(cartItem: CartItemReadDTO): void {
+    const currentCart = this._cart();
+    if (currentCart) {
+      const existingItemIndex = currentCart.cartItems.findIndex(item => item.prizeId === cartItem.prizeId);
+      if (existingItemIndex !== -1) {
+        currentCart.cartItems[existingItemIndex].quantity += cartItem.quantity;
+      } else {
+        currentCart.cartItems.push(cartItem);
+      }
+      this.setCart(currentCart);
+    }
+  }
+  RemovePrizeFromCart(prizeId: number): void {
+    const currentCart = this._cart();
+    if (currentCart) {
+      const updatedCartItems = currentCart.cartItems.filter(item => item.prizeId !== prizeId);
+      this.setCart({ ...currentCart, cartItems: updatedCartItems });
+    }
+  }
+  GetCartByUserId(userId: number): void {
+    this.http.get<ReadCartDTO>(`${this.apiUrl}/GetCartByUserId/${userId}`).subscribe(cart => {
+      this.setCart(cart);
+    });
+  }
 
   setCart(cart: ReadCartDTO): void {
     this._cart.set(cart)
