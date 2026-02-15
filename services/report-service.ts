ee6f924444f549b2c174f8e5fa65f75a3d219f5f
naama_code <naanas344@gmail.com>
2026-02-15 21:27:07 +0200

    add winners & report logic

--- services/report-service.ts
+++ services/report-service.ts
@@ -1,14 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { ReadWinnerDTO } from '../models/Winner';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReportService {
   private http = inject(HttpClient);
-  private readonly apiUrl = 'https://localhost:7156/api/winner';
-  getReportForWinners(token: string | null) {
-    return this.http.get(this.apiUrl+'/GetAllWinners', { headers: { Authorization: `Bearer ${token}` } });
+  private readonly apiUrl = 'https://localhost:7156/api/Winner';
+
+  private _winners = signal<ReadWinnerDTO[] | []>([]);
+  readonly winners = computed(() => this._winners());
+
+  setWinners(winners: ReadWinnerDTO[]) {
+    this._winners.set(winners)
+  }
+
+  getAllWinners(token: string | null): Observable<ReadWinnerDTO[]> {
+
+    if (token === null || token === undefined) {
+      console.log("in ReportService.getAllWinners: token is undefined");
+      throw new Error("in ReportService.getAllWinners: token is undefined")
+
+    }
+    return this.http.get<ReadWinnerDTO[]>(`${this.apiUrl}/GetAllWinners`, { headers: { Authorization: "Bearer " + token } }).pipe(
+      tap((winners: ReadWinnerDTO[]) => this.setWinners(winners)))
+  }
+
+  getRavenue(token: string | null): Observable<number> {
+
+    if (token === null || token === undefined) {
+      console.log("in ReportService.getRavenue: token is undefined");
+      throw new Error("in ReportService.getRavenue: token is undefined")
+
+    }
+    return this.http.get<number>(`${this.apiUrl}/GetRevenue`, { headers: { Authorization: "Bearer " + token } })
+
   }
 
 }
